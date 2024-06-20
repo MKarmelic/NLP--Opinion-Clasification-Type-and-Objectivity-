d5a9314591d9f738f5d0bda5ac1540ce5018416d
MKarmelic <167670728+MKarmelic@users.noreply.github.com>
2024-06-20 01:53:13 -0400

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import re
 import nltk
 from nltk.tokenize import word_tokenize
@@ -6,37 +7,41 @@ from nltk.stem import WordNetLemmatizer
 import joblib
 from sklearn.feature_extraction.text import CountVectorizer
 
-# Load models and CountVectorizer
-logistic_regression_model = joblib.load('/data/Optimized_lr_model.pkl')
-random_forest_model = joblib.load('/data/Optimized_subjective_rf_model.pkl')
-count_vectorizer = joblib.load('/data/count_vectorizer.pkl')
-
 # Initialize NLTK resources
 nltk.download('punkt')
 nltk.download('wordnet')
 
 # Initialize Flask app
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# Define preprocess_text function
+# Load the logistic regression model for sentiment analysis
+logistic_regression_model = joblib.load('data/Optimized_lr_model.pkl')
+
+# Load the random forest regression model for subjectivity analysis
+random_forest_model = joblib.load('data/Optimized_subjective_rf_model.pkl')
+
+# Load the CountVectorizer used during training
+count_vectorizer = joblib.load('data/count_vectorizer.pkl')
+
 def preprocess_text(text):
-    # Preprocessing steps
+    # Define the preprocess_text function
     # Step 1: Remove non-alphabetic characters and convert to lowercase
     text = re.sub("[^a-zA-Z]", " ", text.lower())
-    
+
     # Step 2: Tokenize the text
     tokens = word_tokenize(text)
-    
+
     # Step 3: Lemmatize tokens
     lemma = WordNetLemmatizer()
     tokens = [lemma.lemmatize(word) for word in tokens]
-    
+
     # Step 4: Join tokens back into a string
     processed_text = ' '.join(tokens)
-    
+
     return processed_text
 
-# Define predict_sentiment_and_subjectivity function
 def predict_sentiment_and_subjectivity(preprocessed_text):
     # Transform the preprocessed text into a numeric vector
     numeric_vector = count_vectorizer.transform([preprocessed_text])
@@ -59,7 +64,7 @@ def predict_sentiment_and_subjectivity(preprocessed_text):
 # Route to handle POST requests
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get text from POST request
+    # Get and define the text from POST request
     text = request.json['text']
 
     # Preprocess the text
@@ -70,8 +75,8 @@ def predict():
 
     # Return predictions as JSON
     return jsonify({
-        'sentiment': sentiment,
-        'subjectivity': subjectivity
+        'Sentimiento': sentiment,
+        '% de Subjetividad': subjectivity
     })
 
 if __name__ == '__main__':
